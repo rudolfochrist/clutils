50d507701ea7c0c8cd3dee6e88f07fd7691b12ce
Sebastian Christ <rudolfo.christ@gmail.com>
2014-02-09 15:51:24 +0100

    add some utility functions.

---
+++ utils.lisp
@@ -0,0 +1,24 @@
+(defpackage fyi
+  (:use :cl)
+  (:export :split-string-in-chars
+           :average
+           :median))
+
+(in-package :fyi)
+
+(defun split-string-in-chars (string)
+  "Splits a string in it's charachters."
+  (map 'list #'identity string))
+
+(defun average (elements)
+  (/ (apply #'+ elements) (length elements)))
+
+(defun median (elements)
+  "Determines the median of the given elements. Elements MUST be a sorted list."
+  (let* ((amount (length elements))
+         (index (floor (/ amount 2))))
+   (if (evenp amount)
+       (/ (+ (nth  index elements)
+             (nth (1- index) elements))
+          2)
+       (nth index elements))))
