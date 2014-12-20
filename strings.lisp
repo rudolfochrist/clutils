56d95b549753da54abc16471b171b71fc3dbb200
Sebastian Christ <rudolfo.christ@gmail.com>
2014-12-20 08:04:57 +0100

    Add symbols to :export

--- strings.lisp
+++ strings.lisp
@@ -4,7 +4,13 @@
         :clutils/functional)
   (:export :string-trim-whitespace
            :string-append
-           :string-join))
+           :string-join
+           :string-split
+           :string-starts-with-p
+           :string-ends-with-p
+           :file-to-strings
+           :file-to-string
+           :string-to-file))
 
 (in-package :clutils/strings)
 
@@ -19,20 +25,20 @@
 (defun string-join (list &key (delimiter " "))
   "Joins a list of strings together"
   (string-trim-whitespace (reduce #'(lambda (concatenated current)
-               (concatenate 'string concatenated delimiter (princ-to-string current)))
-           (cdr list)
-           :initial-value (princ-to-string (car list)))))
+                                      (concatenate 'string concatenated delimiter (princ-to-string current)))
+                                  (cdr list)
+                                  :initial-value (princ-to-string (car list)))))
 
 (defun string-split (string &key seperator)
   "Splits a string by provided seperator. If no seperator has been given the string is split into his characters."
   (let ((string-in-chars (map 'list #'identity string)))
     (if seperator
         (nreverse (mapcar (compose #'string-join #'nreverse)
-                 (reduce #'(lambda (result item)
-                             (if (equal item seperator)
-                                 (cons nil result)
-                                 (cons (cons item (car result)) (cdr result))))
-                         string-in-chars :initial-value '())))
+                          (reduce #'(lambda (result item)
+                                      (if (equal item seperator)
+                                          (cons nil result)
+                                          (cons (cons item (car result)) (cdr result))))
+                                  string-in-chars :initial-value '())))
         string-in-chars)))
 
 (defun string-starts-with-p (string prefix)
